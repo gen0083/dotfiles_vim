e62d78e7397e4838cb9f547b0e39dce3d9882c7e
Gen <archiherewego@gmail.com>
2026-05-18 00:33:35 +0900

    refactor: neovimでもautocmdでImeOffを設定するように変更

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- IMEの自動オフ (Vimの function.vim と同等の処理)
+-- macの場合にのみ処理を行う
+if vim.fn.has("mac") == 1 then
+  vim.api.nvim_create_autocmd({ "VimEnter", "InsertLeave", "CmdlineLeave", "FocusGained", "WinEnter" }, {
+    group = vim.api.nvim_create_augroup("ImeAutoOff", { clear = true }),
+    pattern = "*",
+    callback = function()
+      -- インサートモード時には実行しない
+      if vim.fn.mode() ~= "i" then
+        vim.fn.system("macism com.apple.keylayout.US")
+      end
+    end,
+  })
+end
